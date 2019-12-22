379c42d2201e4a8497058ecab09085c1847419da
eliashiv903 <58138902+eliashiv903@users.noreply.github.com>
2019-12-22 23:57:48 +0200

    Update Text.c

--- Text.c
+++ Text.c
@@ -10,10 +10,9 @@ void findLine(char  text[],char  word[]){
     int lengthWord = strlen( word );
     for (int i = 0; i < lengthText; i++) {
         int sumEquals=0;
-        for (int j = i,k=0;apear==0&& j < i+lengthWord &&j<lengthText ; ++j) {
+        for (int j = i,k=0;apear==0&& k < lengthWord &&j<lengthText ; ++j,k++) {
             if(text[j]==word[k]){
                 sumEquals++;
-                k++;
             }
         }
         if(sumEquals==lengthWord)apear=1;
@@ -22,7 +21,7 @@ void findLine(char  text[],char  word[]){
             startLine=i+1;
             apear=0;
         }
-        else if(i==lengthText-1)sysoWordAndLine(startLine, i, text);
+        else if(i==lengthText-1) if(apear==1)sysoWordAndLine(startLine, i, text);
     }
 }
 void sysoWordAndLine (int  start,char  end,char  text[]){
@@ -33,19 +32,22 @@ void sysoWordAndLine (int  start,char  end,char  text[]){
    printf("%s\n", ans);
 }
 void words (char  text[],char  word[]){
+    int sumCarcter=0;
     int lengthText = strlen( text );
     int lengthWord = strlen( word );
     for (int i = 0; i < lengthText; ++i) {
         int sumEquals=0;
-        for (int j = i,k=0; j <= i+lengthWord &&j<lengthText ; ++j) {
+        int j = i;
+        sumCarcter=0;
+        if(j-1!=-1&&text[j-1]!='\t')sumCarcter++;
+        for (int k=0; j <= i+lengthWord &&j<lengthText&&text[j]!=' ' ; ++j) {
             if(text[j]==word[k]){
                 sumEquals++;
                 k++;
             }
         }
+        if(j==lengthText||text[j]!='\t')sumCarcter++;
+        if(sumCarcter!=2) sumEquals=-10;
         if(sumEquals>=lengthWord)	sysoWordAndLine(i, i+lengthWord, text);
     }
-
-
-
 }
