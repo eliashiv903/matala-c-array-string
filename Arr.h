ff2ed933c89534e5f5044a6ac205a5e3a8a87772
eliashiv903 <58138902+eliashiv903@users.noreply.github.com>
2019-12-22 20:41:40 +0200

    Add files via upload

---
+++ Arr.h
@@ -0,0 +1,12 @@
+//
+// Created by eliashiv on 9.11.2019.
+//
+#define num 50
+int arr[num];
+#include <stdio.h>
+#ifndef UNTITLED5_ARR_H
+#define UNTITLED5_ARR_H
+void shift_element(int* arr, int i);
+void print_array(int *pointer, int n);
+void insertion_sort(int* arr,int len);
+#endif //UNTITLED5_ARR_H
